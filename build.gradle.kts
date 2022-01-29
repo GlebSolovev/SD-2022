e13b0b014d99513bfcce33b8b713cc3e7e29a703
dlochmelis33 <dlochmelis@gmail.com>
2022-01-29 21:14:07 +0300

    Add Ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+
 plugins {
     kotlin("jvm") version "1.6.10"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+    id("org.jlleitschuh.gradle.ktlint-idea") version "10.2.1"
 }
 
 group = "org.example"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,1 @@
-rootProject.name = "helpp"
-
+rootProject.name = "Ezh"
